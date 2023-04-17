9497114d7fa2d03a89da042ab21de0492457211b
Saman Hosseini <bounoable@gmail.com>
2023-04-17 17:15:26 +0200

    feat(openai): add support for custom model selection

--- go/TODO.md
+++ go/TODO.md
@@ -1,6 +1,5 @@
 # To-Do
 
-- Add `model` option to OpenAI service
 - Parallelize generations
 - Make `opendocs.Repository` a string
 - Fix `patch.splitString()` function to correctly handle multiple spaces
--- go/services/openai/openai.go
+++ go/services/openai/openai.go
@@ -11,15 +11,18 @@ import (
 	"golang.org/x/exp/slog"
 )
 
+const DefaultModel = openai.GPT3TextDavinci003
+
 var _ generate.Service = (*Service)(nil)
 
 var systemPrompt = `You are DocGPT, a code documentation writer.` +
-	`You are given a file list, a file name, the source code of that file, and an identifier. ` +
+	`You are given a a file name, the source code of that file, and an identifier. ` +
 	`Using these, you will write the documentation for the type or function identified by the identifier. ` +
 	`You will write the documentation in GoDoc format.`
 
 type Service struct {
 	client *openai.Client
+	model  string
 	log    *slog.Logger
 }
 
@@ -37,6 +40,12 @@ func WithClient(c *openai.Client) Option {
 	}
 }
 
+func Model(model string) Option {
+	return func(s *Service) {
+		s.model = model
+	}
+}
+
 func New(apiKey string, opts ...Option) *Service {
 	client := openai.NewClient(apiKey)
 	return NewFrom(append([]Option{WithClient(client)}, opts...)...)
@@ -47,13 +56,16 @@ func NewFrom(opts ...Option) *Service {
 	for _, opt := range opts {
 		opt(&svc)
 	}
+	if svc.model == "" {
+		svc.model = DefaultModel
+	}
 	if svc.log == nil {
 		svc.log = internal.NopLogger()
 	}
 	return &svc
 }
 
-func (g *Service) GenerateDoc(ctx generate.Context) (string, error) {
+func (svc *Service) GenerateDoc(ctx generate.Context) (string, error) {
 	files := ctx.Files()
 	file := ctx.File()
 	identifier := ctx.Identifier()
@@ -62,7 +74,7 @@ func (g *Service) GenerateDoc(ctx generate.Context) (string, error) {
 		return "", err
 	}
 
-	answer, err := g.createCompletion(ctx, files, file, identifier, code)
+	answer, err := svc.createCompletion(ctx, files, file, identifier, code)
 	if err != nil {
 		return "", fmt.Errorf("create completion: %w", err)
 	}
@@ -70,77 +82,108 @@ func (g *Service) GenerateDoc(ctx generate.Context) (string, error) {
 	return answer, nil
 }
 
-func (g *Service) createCompletion(
+func (svc *Service) createCompletion(
 	ctx context.Context,
 	files []string,
 	file,
 	longIdentifier string,
 	code []byte,
 ) (string, error) {
-	// filesPrompt := filesPrompt(files)
-
 	identifier := normalizeIdentifier(longIdentifier)
 	msg := prompt(file, identifier, longIdentifier, code)
 
-	g.log.Debug("[OpenAI] Creating chat completion ...", "file", file, "identifier", identifier)
-
-	resp, err := g.client.CreateCompletion(ctx, openai.CompletionRequest{
-		Model:            openai.GPT3TextDavinci003,
+	req := openai.CompletionRequest{
+		Model:            svc.model,
 		TopP:             0.2,
 		MaxTokens:        512,
 		PresencePenalty:  0.1,
 		FrequencyPenalty: 0.1,
 		Prompt:           msg,
-	})
+	}
 
-	// resp, err := g.client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
-	// 	Model:            openai.GPT3Dot5Turbo,
-	// 	Temperature:      0.618,
-	// 	MaxTokens:        512,
-	// 	PresencePenalty:  0.1,
-	// 	FrequencyPenalty: 0.2,
-	// 	Messages: []openai.ChatCompletionMessage{
-	// 		{
-	// 			Role:    openai.ChatMessageRoleSystem,
-	// 			Content: systemPrompt,
-	// 		},
-	// 		{
-	// 			Role:    openai.ChatMessageRoleUser,
-	// 			Content: filesPrompt,
-	// 		},
-	// 		{
-	// 			Role:    openai.ChatMessageRoleUser,
-	// 			Content: msg,
-	// 		},
-	// 	},
-	// })
+	svc.log.Debug("[OpenAI] Creating completion ...", "file", file, "identifier", identifier, "model", req.Model)
+
+	generate := svc.useModel(req.Model)
+	answer, err := generate(ctx, req)
 	if err != nil {
-		return "", fmt.Errorf("create chat completion: %w", err)
+		return "", err
 	}
-
-	if len(resp.Choices) == 0 {
-		return "", fmt.Errorf("openai: no choices returned")
-	}
-
-	answer := resp.Choices[0].Text
 	answer = normalizeAnswer(answer)
 
-	g.log.Debug("[OpenAI] Documentation generated", "file", file, "identifier", identifier, "docs", answer)
+	svc.log.Debug("[OpenAI] Documentation generated", "file", file, "identifier", identifier, "docs", answer)
 
 	return answer, nil
+}
 
-	// choice := resp.Choices[0]
-	// if choice.FinishReason != "stop" {
-	// 	return choice, fmt.Errorf("openai: unexpected finish reason: %q", choice.FinishReason)
-	// }
+func (svc *Service) useModel(model string) func(context.Context, openai.CompletionRequest) (string, error) {
+	if isChatModel(model) {
+		return svc.createChatCompletion
+	}
 
-	// if choice.Message.Role != openai.ChatMessageRoleAssistant {
-	// 	return choice, fmt.Errorf("openai: unexpected message role in answer: %q", choice.Message.Role)
-	// }
+	return func(ctx context.Context, req openai.CompletionRequest) (string, error) {
+		resp, err := svc.client.CreateCompletion(ctx, req)
+		if err != nil {
+			return "", err
+		}
 
-	// g.log.Debug("[OpenAI] Documentation generated", "file", file, "identifier", identifier, "docs", choice.Message.Content)
+		if len(resp.Choices) == 0 {
+			return "", fmt.Errorf("openai: no choices returned")
+		}
 
-	// return choice, nil
+		choice := resp.Choices[0]
+		if choice.FinishReason != "stop" {
+			return choice.Text, fmt.Errorf("openai: unexpected finish reason: %q", choice.FinishReason)
+		}
+
+		return choice.Text, nil
+	}
+}
+
+func (svc *Service) createChatCompletion(ctx context.Context, req openai.CompletionRequest) (string, error) {
+	resp, err := svc.client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model:            openai.GPT3Dot5Turbo,
+		Temperature:      req.Temperature,
+		MaxTokens:        req.MaxTokens,
+		PresencePenalty:  req.PresencePenalty,
+		FrequencyPenalty: req.FrequencyPenalty,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: systemPrompt,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: req.Prompt.(string),
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	choice := resp.Choices[0]
+	if choice.FinishReason != "stop" {
+		return choice.Message.Content, fmt.Errorf("openai: unexpected finish reason: %q", choice.FinishReason)
+	}
+
+	if choice.Message.Role != openai.ChatMessageRoleAssistant {
+		return choice.Message.Content, fmt.Errorf("openai: unexpected message role in answer: %q", choice.Message.Role)
+	}
+
+	return choice.Message.Content, nil
+}
+
+var chatModels = map[string]bool{
+	openai.GPT4:              true,
+	openai.GPT40314:          true,
+	openai.GPT432K:           true,
+	openai.GPT432K0314:       true,
+	openai.GPT3Dot5Turbo:     true,
+	openai.GPT3Dot5Turbo0301: true,
+}
+
+func isChatModel(model string) bool {
+	return chatModels[model]
 }
 
 func normalizeIdentifier(identifier string) string {
@@ -151,21 +194,7 @@ func normalizeIdentifier(identifier string) string {
 	return parts[1]
 }
 
-// func filesPrompt(files []string) string {
-// 	var sb strings.Builder
-// 	sb.WriteString("Files:")
-
-// 	for _, f := range files {
-// 		sb.WriteString("\n- ")
-// 		sb.WriteString(f)
-// 	}
-
-// 	return sb.String()
-// }
-
 func prompt(file, identifier, longIdentifier string, code []byte) string {
-	// "Write a short documentation for %q in idiomatic GoDoc format, with references to symbols wrapped within brackets. Only output the documentation, not the input code. Do not include examples. Do not describe any other symbols besides %q. Keep it as short as possible while not being too unspecific. Start the first sentence with %q. This is the source code of %q:\n%s",
-
 	return fmt.Sprintf(
 		"Create a concise documentation for %q in GoDoc format, with references to symbols wrapped within brackets. Provide only the documentation, excluding the input code and examples. Begin the first sentence with %q. Maintain brevity without sacrificing specificity. Here is the source code for %q:\n%s",
 		longIdentifier,
