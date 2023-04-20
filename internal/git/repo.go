114fb686fd15388abbd2e44aeae7fcaaafe82350
Saman Hosseini <bounoable@gmail.com>
2023-04-20 04:26:06 +0200

    fix(git): use CombinedOutput instead of Output to capture both stdout and stderr in Git.Cmd method

--- internal/git/repo.go
+++ internal/git/repo.go
@@ -16,7 +16,7 @@ type Git string
 func (g Git) Cmd(args ...string) (*exec.Cmd, []byte, error) {
 	cmd := exec.Command("git", args...)
 	cmd.Dir = string(g)
-	out, err := cmd.Output()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return cmd, out, fmt.Errorf("git: %s (%w)", out, err)
 	}
