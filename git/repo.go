40866062c22e65d729d63648fdd927699a48fe95
Saman Hosseini <bounoable@gmail.com>
2023-04-28 22:11:42 +0200

    feat(repo.go): add context.Context to Commit method test(repo_test.go): update test to use patch.Mock and add context.Context to Commit call

--- git/repo.go
+++ git/repo.go
@@ -1,6 +1,7 @@
 package git
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"time"
@@ -10,54 +11,28 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-// Patch represents a patch that can be applied to a Git repository. It is an
-// interface that defines the Apply method. The Apply method takes a root string
-// as argument, which specifies the root directory of the Git repository, and
-// returns an error if the patch application fails.
 type Patch interface {
-	Apply(root string) error
+	Apply(ctx context.Context, root string) error
 }
 
-// Committer represents an interface for a type that can commit changes to a Git
-// repository. Any type implementing this interface must have a Commit method
-// which returns a Commit. The Repository type has a Commit method which takes a
-// Patch and optional CommitOptions and applies the patch to the repository,
-// creates a new branch if necessary, adds the changes to the index, and commits
-// them with the specified commit message. If the provided patch implements the
-// Committer interface, its Commit method will be used to create the commit
-// message.
 type Committer interface {
 	Commit() Commit
 }
 
-// Repository represents a Git repository. It provides methods for committing
-// patches and accessing the root directory of the repository. The Repository
-// type implements the Patch and Committer interfaces, allowing for patches to
-// be applied to the repository and commits to be made on behalf of the
-// repository, respectively. It can be initialized with options using Repo
-// function.
 type Repository struct {
 	root string
 	git  git.Git
 	log  *slog.Logger
 }
 
-// Option is a type that represents an option for a Repository. It is used to
-// configure Repository instances with optional parameters. Option functions
-// modify the Repository instance passed to them.
 type Option func(*Repository)
 
-// WithLogger returns an Option that sets the logger for a Repository. The
-// logger is used to log information about the git commands that are run during
-// patching.
 func WithLogger(h slog.Handler) Option {
 	return func(repo *Repository) {
 		repo.log = slog.New(h)
 	}
 }
 
-// Repo represents a Git repository. It provides methods for committing patches
-// to the repository. Use the Repo function to create a new Repository.
 func Repo(root string, opts ...Option) *Repository {
 	repo := &Repository{
 		root: root,
@@ -72,21 +47,12 @@ func Repo(root string, opts ...Option) *Repository {
 	return repo
 }
 
-// Root returns the root directory of the Repository [Repository].
 func (repo *Repository) Root() string {
 	return repo.root
 }
 
-// CommitOption is a type representing a function that modifies the behavior of
-// the Commit function in Repository. It can be used as an optional argument in
-// the Commit function to specify additional options such as the branch to
-// commit to. The Branch function returns a CommitOption that sets the branch
-// name for the commit.
 type CommitOption func(*commit)
 
-// Branch is a function that returns a CommitOption. The returned option sets
-// the name of the branch to commit to when committing a patch. If not set, the
-// branch name defaults to "jotbot-patch".
 func Branch(branch string) CommitOption {
 	return func(c *commit) {
 		c.branch = branch
@@ -97,13 +63,7 @@ type commit struct {
 	branch string
 }
 
-// Commit commits a patch to the repository. It takes a Patch and optional
-// CommitOptions. If no branch is specified in the options, "jotbot-patch" is
-// used. If the branch already exists, the name will be suffixed with the
-// current Unix timestamp in milliseconds. The patch is applied to the
-// repository, and all changes are added and committed. If the Patch implements
-// Committer, its Commit method will be called to obtain commit information.
-func (r *Repository) Commit(p Patch, opts ...CommitOption) error {
+func (r *Repository) Commit(ctx context.Context, p Patch, opts ...CommitOption) error {
 	var cfg commit
 	for _, opt := range opts {
 		opt(&cfg)
@@ -124,7 +84,7 @@ func (r *Repository) Commit(p Patch, opts ...CommitOption) error {
 		return fmt.Errorf("checkout branch: %w: %s", err, string(output))
 	}
 
-	if err := p.Apply(r.root); err != nil {
+	if err := p.Apply(ctx, r.root); err != nil {
 		return fmt.Errorf("apply patch to repository %s: %w", r.root, err)
 	}
 
--- git/repo_test.go
+++ git/repo_test.go
@@ -1,18 +1,16 @@
 package git_test
 
 import (
-	"io"
-	"io/fs"
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/modernice/jotbot/git"
 	"github.com/modernice/jotbot/internal/git/gittest"
+	"github.com/modernice/jotbot/internal/patch"
 	"github.com/modernice/jotbot/internal/tests"
-	"github.com/modernice/jotbot/langs/golang"
-	"github.com/psanford/memfs"
 )
 
 var (
@@ -21,63 +19,38 @@ var (
 )
 
 func TestRepo_Commit(t *testing.T) {
-	tests.WithRepo("basic", repoRoot, func(repoFS fs.FS) {
-		codeFile, err := repoFS.Open("foo.go")
-		if err != nil {
-			t.Fatal(err)
-		}
+	repo := git.Repo(repoRoot)
 
-		code, err := io.ReadAll(codeFile)
-		if err != nil {
-			t.Fatal(err)
-		}
+	p := patch.Mock(map[string]string{
+		"foo.go": heredoc.Doc(`
+			package foo
 
-		sourceFS := memfs.New()
-		if err := sourceFS.WriteFile("foo.go", code, fs.ModePerm); err != nil {
-			t.Fatal(err)
-		}
-
-		p := golang.NewPatch(sourceFS)
-		if err := p.Comment("foo.go", "Foo", `Foo is a function that returns a "foo" error.`); err != nil {
-			t.Fatal(err)
-		}
-
-		repo := git.Repo(repoRoot)
-
-		if err := repo.Commit(p); err != nil {
-			t.Fatal(err)
-		}
-
-		g.AssertBranchPrefix(t, "jotbot-patch")
-		g.AssertCommit(t, git.Commit{
-			Msg: "docs: add missing documentation",
-			Desc: []string{
-				"Updated docs:",
-				"  - foo.go@Foo",
-			},
-			Footer: "This commit was created by jotbot.",
-		})
-
-		dryRun, err := p.DryRun()
-		if err != nil {
-			t.Fatal(err)
-		}
-		wantCode, ok := dryRun["foo.go"]
-		if !ok {
-			t.Fatal("no code for foo.go in dry run result")
-		}
-
-		gotCodeFile, err := repoFS.Open("foo.go")
-		if err != nil {
-			t.Fatal(err)
-		}
-		gotCode, err := io.ReadAll(gotCodeFile)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if string(gotCode) != string(wantCode) {
-			t.Fatalf("unexpected code\n%s", cmp.Diff(string(wantCode), string(gotCode)))
-		}
+			// Foo does nothing.
+			func Foo() {}
+		`),
 	})
+
+	if err := repo.Commit(context.Background(), p); err != nil {
+		t.Fatal(err)
+	}
+
+	g.AssertBranchPrefix(t, "jotbot-patch")
+	g.AssertCommit(t, git.Commit{
+		Msg: "docs: add missing documentation",
+		Desc: []string{
+			"Updated docs:",
+			"  - foo.go@Foo",
+		},
+		Footer: "This commit was created by jotbot.",
+	})
+
+	repoFS := os.DirFS(repoRoot)
+
+	f, err := repoFS.Open("foo.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	tests.ExpectComment(t, "Foo", "Foo does nothing.", f)
 }
