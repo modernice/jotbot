fe0282437b3d286071ca055dbb077b0d31459242
Saman Hosseini <bounoable@gmail.com>
2023-05-05 16:15:59 +0200

    feat(golang): add function to generate target name for documentation prompt

--- langs/golang/prompt.go
+++ langs/golang/prompt.go
@@ -2,6 +2,7 @@ package golang
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/modernice/jotbot/generate"
 )
@@ -9,8 +10,26 @@ import (
 func Prompt(input generate.Input) string {
 	return fmt.Sprintf(
 		"Write a concise documentation for %s in GoDoc format. Do not output any links. Do not output the source code code or code examples. Use brackets to enclose symbol references. Start the first sentence with %q. Write in the style of Go library documentation. This is the source code of the file:\n\n%s",
-		"", // TODO(bouoable): Generate target from identifier
+		Target(input.Identifier),
 		fmt.Sprintf("%s ", input.Identifier),
 		input.Code,
 	)
+}
+
+func Target(identifier string) string {
+	parts := strings.Split(identifier, ":")
+	if len(parts) != 2 {
+		return identifier
+	}
+
+	switch parts[0] {
+	case "func":
+		return fmt.Sprintf("function '%s'", parts[1])
+	case "type":
+		return fmt.Sprintf("type '%s'", parts[1])
+	case "var":
+		return fmt.Sprintf("variable '%s'", parts[1])
+	default:
+		return fmt.Sprintf("'%s'", identifier)
+	}
 }
