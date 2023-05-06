113c28b8297c6a025ed66810440d348fa761c17b
Saman Hosseini <bounoable@gmail.com>
2023-05-07 00:55:07 +0200

    docs(reset.go): remove comment

--- tools/reset/reset.go
+++ tools/reset/reset.go
@@ -2,12 +2,6 @@ package reset
 
 import "github.com/dave/dst"
 
-// Comments [func Comments(node dst.Node)] is a function that removes comments 
-// from the specified Go Abstract Syntax Tree (AST) node. This function accepts 
-// a node of type dst.Node and recursively traverses the AST to remove all 
-// comments associated with the specified node. The types of nodes that have 
-// their comments removed include *dst.File, *dst.GenDecl, *dst.FuncDecl, 
-// *dst.ValueSpec, and *dst.TypeSpec.
 func Comments(node dst.Node) {
 	dst.Inspect(node, func(node dst.Node) bool {
 		switch node := node.(type) {
