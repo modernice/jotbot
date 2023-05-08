0916f1d92b8acba23f5567b0c85100a69cf98fde
Saman Hosseini <bounoable@gmail.com>
2023-05-08 17:01:12 +0200

    chore(jotbot-ts): refactor release script to use async/await and add support for git tagging and pushing
    This commit refactors the release script to use async/await instead of execSync and adds support for git tagging and pushing. The script now uses the promisify function from the util module to convert the exec function to a promise-based function. The execute function is now an async function that uses the promisified exec function to execute commands. The script now adds support for git tagging and pushing by adding a new commit with a message that includes the new version number and pushing the changes to the remote repository.

--- packages/jotbot/scripts/release.ts
+++ packages/jotbot/scripts/release.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url'
-import { execSync } from 'node:child_process'
+import { promisify } from 'node:util'
+import { exec } from 'node:child_process'
 import { SemanticVersion } from '@hediet/semver'
 import { readPackageJSON } from 'pkg-types'
 
@@ -7,21 +8,41 @@ const packageRoot = fileURLToPath(new URL('..', import.meta.url))
 
 export async function release(version: string) {
   const pkg = await readPackageJSON(packageRoot)
-  const currentVersion = SemanticVersion.parse(pkg.version!)
-  const newVersion = SemanticVersion.parse(version)
+  const currentVersion = SemanticVersion.parse(pkg.version!.replace('v', ''))
+  const newVersion = SemanticVersion.parse(version.replace('v', ''))
 
   if (newVersion.compareTo(currentVersion) < 1) {
     throw new Error(
-      `New version ${newVersion} must be greater than current version ${currentVersion}.`,
+      `New version v${newVersion} must be greater than current version v${currentVersion}.`,
     )
   }
 
+  await execute('pnpm i')
+  await execute('pnpm build')
+
   const v = newVersion.toString()
-  execSync(`pnpm version ${v}`)
+  await execute(`pnpm version v${v}`)
 
-  execSync('git add package.json')
-  execSync(`git commit -m 'chore(jotbot-ts): ${v}'`)
-  execSync(`git push`)
+  await execute('git add package.json')
+  await execute(`git commit -m 'chore(jotbot-ts): v${v}'`)
+  await execute(`git push`)
 
-  execSync(`pnpm publish --access public`)
+  await execute(`pnpm publish --access public --no-git-checks`)
+}
+
+const _exec = promisify(exec)
+async function execute(cmd: string) {
+  const { stdout, stderr } = await _exec(cmd)
+  if (stderr) {
+    throw new Error(stderr)
+  }
+  process.stdout.write(stdout)
+  return stdout
+}
+
+if (import.meta.url.startsWith('file:')) {
+  const modulePath = fileURLToPath(import.meta.url)
+  if (process.argv.length > 2 && process.argv[1] === modulePath) {
+    release(process.argv[2])
+  }
 }
